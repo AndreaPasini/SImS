591c8ba663d304f8a7d0141571f4076ea32e0ee8
andrea <andrea.pasini@polito.it>
2019-06-08 15:15:06 +0200

    Parallelism.

--- inference.py
+++ inference.py
@@ -76,9 +76,9 @@ def run_model(img_name, i):
 
 class FuncThread(threading.Thread):
     def __init__(self, target, *args):
+        threading.Thread.__init__(self)
         self._target = target
         self._args = args
-        threading.Thread.__init__(self)
 
     def run(self):
         self._target(*self._args)

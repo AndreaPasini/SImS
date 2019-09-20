adcd843c22108023f1538cd62d206c083442ded7
Antonio Ventura <antovent@hotmail.it>
2019-09-20 12:34:50 +0200

    apportate megliorie e aggiunto step 5 con creazione del file features.csv

--- main_sad.py
+++ main_sad.py
@@ -7,8 +7,6 @@
 from datetime import datetime
 import os
 from shutil import rmtree
-import sys
-import csv
 import random
 import pandas as pd
 from itertools import groupby
@@ -20,88 +18,101 @@ import PIL.Image as Image
 from panopticapi.utils import load_png_annotation
 import numpy as np
 import pyximport;
+
 pyximport.install(language_level=3)
 from semantic_analysis.algorithms import image2strings, compute_string_positions
 
-
 def is_on(vector, first_i, first_j):
-    return first_i+1==first_j
+    return first_i + 1 == first_j
 
-def analyze_image(image_name, segments_info, image_id, annot_folder, df):
-    #Load png annotation
+def analyze_image(image_name, segments_info, image_id, annot_folder):
+    # Load png annotation
     img_ann = load_png_annotation(os.path.join(annot_folder, image_name))
     strings = image2strings(img_ann)
     positions = compute_string_positions(strings)
-    rand = random.choice(list(positions.keys()))
+    rand = random.choice(list(positions.items()))
 
-    img = Image.open('../COCO/annotations/panoptic_val2017'+'/'+image_name)
+    img = Image.open('../COCO/annotations/panoptic_val2017' + '/' + image_name)
+    subject = rand[0][0] #yellow
+    reference = rand[0][1] #blue
 
-    subject = rand[0]
-    reference = rand[1]
+    trainingRow = [image_id, subject, reference, '']
+    addRowinCSV("Training/ImageDetails.csv", trainingRow)
+    featuresRow = [image_id, subject, reference] + extractDict(rand[1])
+    addRowinCSV("Training/Features.csv", featuresRow)
 
-    rS, gS, bS  = convertToRGB(subject)
-    rR, gR, bR  = convertToRGB(reference)
+    imgSub = changeColor(img, convertToRGB(subject), True)
+    imgRef = changeColor(imgSub, convertToRGB(reference), False)
+    imgRef.save('Training/' + image_name, 'PNG')
 
+    print("Done")
+
+def addRowinCSV(path, row):
+    df = pd.read_csv(path, sep=';', encoding="utf-8")
+    df.loc[df.index.max() + 1] = row
+    df.to_csv(path, sep=';', index=False)
+
+def changeColor(img, rgbSub, firstImage):
     img = img.convert('RGBA')
-    dataS = np.array(img)
-    red, green, blue, alpha = dataS.T  # Temporarily unpack the bands for readability
-    # Replace white with yellow... (leaves alpha values alone...)
-    subject_areas = (red == rS) & (green == gS) & (blue == bS)
-    dataS[..., :-1][subject_areas.T] = (128, 128, 0)  # Transpose back needed
-    imgS = Image.fromarray(dataS)
+    data = np.array(img)
+    red, green, blue, alpha = data.T  # Temporarily unpack the bands for readability
+    subject_areas = (red == rgbSub[0]) & (green == rgbSub[1]) & (blue == rgbSub[2])
+    data[..., :-1][subject_areas.T] = (128, 128, 0) if firstImage else (0, 0, 255)  # Transpose back needed
+    img = Image.fromarray(data)
+    return img
 
-    img2 = imgS.convert('RGBA')
-    dataR = np.array(img2)
-    red, green, blue, alpha = dataR.T  # Temporarily unpack the bands for readability
-    # Replace white with blue... (leaves alpha values alone...)
-    reference_areas = (red == rR) & (green == gR) & (blue == bR)
-    dataR[..., :-1][reference_areas.T] = (0, 0, 255)  # Transpose back needed
-    imgR = Image.fromarray(dataR)
-
-    imgR.save('Features/'+image_name, 'PNG')
-
-    df = pd.read_csv("Features/ImageDetails.csv", sep=',', encoding="utf-8")
-    df.loc[df.index.max() + 1] = [image_id, subject, reference, '']
-    df.to_csv("Features/ImageDetails.csv", index=False)
-
+def extractDict(d):
+    features = []
+    for k, v in d.items():
+        features.append(v)
+    return features
 
 def convertToRGB(decimalColor):
     b = decimalColor & 255
     g = (decimalColor >> 8) & 255
     r = (decimalColor >> 16) & 255
-    return b, g, r
+    return (b, g, r)
 
 def inizializeCSV():
-    if not os.path.exists('Features/'):
-        os.mkdir('Features')
+    if not os.path.exists('Training'):
+        os.mkdir('Training')
     else:
-        rmtree('Features')
-        os.mkdir('Features')
+        rmtree('Training')
+        os.mkdir('Training')
+
     data = {'image_id': [], 'Subject': [], 'Reference': [], 'Position': []}
     df = pd.DataFrame(data, columns=['image_id', 'Subject', 'Reference', 'Position'])
-    df.to_csv('Features/ImageDetails.csv', index=None, header=True)
-    return df
+    df.to_csv('Training/ImageDetails.csv', sep=';', index=None, header=True)
+    print("Create ImageDetails.csv")
 
-def run_tasks(json_file, annot_folder, df):
+    dataFeatures = {'image_id': [], 'Subject': [], 'Reference': [], 'i on j': [], 'j on i': [], 'i above j': [],
+                    'j above i': [], 'i around j': [], 'j around i': [], 'other': []}
+    df = pd.DataFrame(dataFeatures, columns=['image_id', 'Subject', 'Reference', 'i on j', 'j on i', 'i above j',
+                                             'j above i', 'i around j', 'j around i', 'other'])
+    df.to_csv('Training/Features.csv', sep=';', index=None, header=True)
+    print("Create Features.csv")
+
+def run_tasks(json_file, annot_folder):
     """
     Run tasks: analyze training annotations
     :param json_file: annotation file with classes for each segment
     :param annot_folder: folder with png annotations
     """
-    #Load annotations
+    # Load annotations
     with open(json_file, 'r') as f:
         json_data = json.load(f)
         annot_dict = {}
         id_dict = {}
         for img_ann in json_data['annotations']:
-            annot_dict[img_ann['file_name']]=img_ann['segments_info']
+            annot_dict[img_ann['file_name']] = img_ann['segments_info']
         for img_ann in json_data['annotations']:
-            id_dict[img_ann['file_name']]=img_ann['image_id']
-    #Get files to be analyzed
+            id_dict[img_ann['file_name']] = img_ann['image_id']
+    # Get files to be analyzed
     files = sorted(listdir(annot_folder))
 
-    #Init progress bar
+    # Init progress bar
     pbar = tqdm(total=len(files))
+
     def update(x):
         pbar.update()
 
@@ -110,37 +121,36 @@ def run_tasks(json_file, annot_folder, df):
 
     pool = Pool(num_processes)
     for img in files:
-        pool.apply_async(analyze_image, args=(img, annot_dict[img], id_dict[img], annot_folder, df), callback=update)
+        pool.apply_async(analyze_image, args=(img, annot_dict[img], id_dict[img], annot_folder), callback=update)
     pool.close()
     pool.join()
     pbar.close()
-
     print("Done")
 
 
 def example2():
     start_time = datetime.now()
 
-    lista=[]
-    #TODO: use training images, instead of validation
-    for i in range(0,100000):
-        a=100*100
+    lista = []
+    # TODO: use training images, instead of validation
+    for i in range(0, 100000):
+        a = 100 * 100
     lista.append(a)
 
     end_time = datetime.now()
     print("Done.")
     print('Duration: ' + str(end_time - start_time))
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     start_time = datetime.now()
-    chunck_size = 10    # number of images processed for each task
+    chunck_size = 10  # number of images processed for each task
     num_processes = 10  # number of processes where scheduling tasks
 
-    #TODO: use training images, instead of validation
+    # TODO: use training images, instead of validation
     input_images = '../COCO/images/val2017/'
-    df = inizializeCSV()
-    run_tasks('../COCO/annotations/panoptic_val2017.json', '../COCO/annotations/panoptic_val2017', df)
+    inizializeCSV()
+    run_tasks('../COCO/annotations/panoptic_val2017.json', '../COCO/annotations/panoptic_val2017')
     end_time = datetime.now()
     print("Done.")
     print('Duration: ' + str(end_time - start_time))

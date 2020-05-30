92d5ecd5c5dfd9b51195b7a6290b0b3817762c05
andrea <andrea.pasini@polito.it>
2020-05-30 10:36:54 +0200

    Competitors Summarization.

--- main_competitors.py
+++ main_competitors.py
@@ -8,7 +8,7 @@ import pandas as pd
 from datetime import datetime
 competitors_dir = '../COCO/competitors/'
 
-def get_SIFT(img, sift):
+def __get_SIFT(img, sift):
     """
     Return SIFT descriptors for this image (resized 200x200)
     """
@@ -17,7 +17,7 @@ def get_SIFT(img, sift):
     (kps, descs) = sift.detectAndCompute(img_gray, None)
     return kps, descs
 
-def get_descriptors(img_names):
+def __get_descriptors(img_names):
     """
     Return SIFT descriptors matrix (N. images x 100 x 128)
     Max 100 descriptors for each image
@@ -26,7 +26,7 @@ def get_descriptors(img_names):
     sift = cv2.xfeatures2d.SIFT_create()
     for i, img_name in enumerate(img_names):
         img = cv2.imread(os.path.join(COCO_img_train_dir, img_name))
-        kps, descs = get_SIFT(img, sift)
+        kps, descs = __get_SIFT(img, sift)
         if descs is not None and len(descs)>0:
             if X is None:
                 X = descs[:100]
@@ -35,11 +35,18 @@ def get_descriptors(img_names):
     return X
 
 def get_BOW(img_names, codebook):
+    """
+    Compute Bag Of Words features for all the specified images
+    Each image is described with a normalized histograms that counts the presence of the 500 SIFT descriptors
+    :param img_names: list of image names (COCO train)
+    :param codebook: Kmeans model with SIFT features codebook (500 elements/centroids)
+    :return: features matrix (n_images x 500)
+    """
     X = None
     sift = cv2.xfeatures2d.SIFT_create()
     for i, img_name in enumerate(img_names):    # 34 secondi con 1000 immagini
         img = cv2.imread(os.path.join(COCO_img_train_dir, img_name))
-        kps, descs = get_SIFT(img, sift)
+        kps, descs = __get_SIFT(img, sift)
         fvect = np.zeros(codebook.n_clusters)
         if descs is not None and len(descs)>0:
             y = codebook.predict(descs)
@@ -55,37 +62,51 @@ def get_BOW(img_names, codebook):
             print(f"Done: {i}")
     return X
 
-images = os.listdir(COCO_img_train_dir)
-# selected = np.random.choice(images, 10000, replace=False)
-# # Computing descriptors
-# print("Computing descriptors...")
-# start_time = datetime.now()
-# X = get_descriptors(selected)
-# X.dump(os.path.join(competitors_dir, "sift_descr_collection.np"))
-# print("Saved.")
-# end_time = datetime.now()
-# print('Duration: ' + str(end_time - start_time))
-#
-# print("Computing codebook with KMeans...")
-# start_time = datetime.now()
-# X = np.load(os.path.join(competitors_dir, "sift_descr_collection.np"),allow_pickle=True)
-# print(f"Initial data: {X.shape[0]}")
-# X = X[np.random.choice(X.shape[0], 100000, replace=False), :] # 100K samples
-# print(f"Sampled data: {X.shape[0]}")
-# codebook = KMeans(500) # Number of codes
-# y = codebook.fit_transform(X)
-# dump(codebook, os.path.join(competitors_dir, "sift_codebook.pkl"))
-# print("Saved.")
-# end_time = datetime.now()
-# print('Duration: ' + str(end_time - start_time))
+def compute_BOW_descriptors():
+    """
+    Compute Bag Of Words features for COCO train images
+    Each image is described with a normalized histograms that counts the presence of the 500 SIFT descriptors
+    """
+    images = os.listdir(COCO_img_train_dir)
+    selected = np.random.choice(images, 10000, replace=False)
+    # Computing descriptors
+    print("Computing descriptors...")
+    start_time = datetime.now()
+    X = __get_descriptors(selected)
+    X.dump(os.path.join(competitors_dir, "sift_descr_collection.np"))
+    print("Saved.")
+    end_time = datetime.now()
+    print('Duration: ' + str(end_time - start_time))
 
-print("Computing feature vectors for all images...")
-start_time = datetime.now()
-codebook = load(os.path.join(competitors_dir, "sift_codebook.pkl"))
-X = get_BOW(images, codebook)
-df = pd.DataFrame(X)
-df.to_csv(os.path.join(competitors_dir, "bow_images.np"), index=False)
-X.dump(os.path.join(competitors_dir, "bow_images.np"))
-print("Saved.")
-end_time = datetime.now()
-print('Duration: ' + str(end_time - start_time))
+    print("Computing codebook with KMeans...")
+    start_time = datetime.now()
+    X = np.load(os.path.join(competitors_dir, "sift_descr_collection.np"),allow_pickle=True)
+    print(f"Initial data: {X.shape[0]}")
+    X = X[np.random.choice(X.shape[0], 100000, replace=False), :] # 100K samples
+    print(f"Sampled data: {X.shape[0]}")
+    codebook = KMeans(500) # Number of codes
+    y = codebook.fit_transform(X)
+    dump(codebook, os.path.join(competitors_dir, "sift_codebook.pkl"))
+    print("Saved.")
+    end_time = datetime.now()
+    print('Duration: ' + str(end_time - start_time))
+
+    print("Computing feature vectors for all images...")
+    start_time = datetime.now()
+    codebook = load(os.path.join(competitors_dir, "sift_codebook.pkl"))
+    X = get_BOW(images, codebook)
+    df = pd.DataFrame(X)
+    df.to_csv(os.path.join(competitors_dir, "bow_images.pd"), index=False)
+    X.dump(os.path.join(competitors_dir, "bow_images.np"))
+    print("Saved.")
+    end_time = datetime.now()
+    print('Duration: ' + str(end_time - start_time))
+
+if __name__ == "__main__":
+    # Feature extraction for each image
+    #compute_BOW_descriptors()
+    X = np.load(os.path.join(competitors_dir, "bow_images.np"))
+    df = pd.DataFrame(X)
+    df.to_csv(os.path.join(competitors_dir, "bow_images.pd"), index=False)
+    #X = pd.read_csv(os.path.join(competitors_dir, "bow_images.np"), index_col=False)
+    #print(len(X))
